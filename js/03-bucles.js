955b69088fa8b59caf94889a3ebce7a0993d6426
Gonzalo G. <gonzalogarcia035@gmail.com>
2025-06-30 22:38:05 -0300

    se corrigio ej 3

--- js/03-bucles.js
+++ js/03-bucles.js
@@ -1,18 +1,39 @@
-let resultado = "";
-let ingreso = prompt("Introduce una cadena de texto (pulsa Cancelar para salir):");
+//let resultado = "";
+//let ingreso = prompt("Introduce una cadena de texto (pulsa Cancelar para salir):");
 
-while (ingreso !== null) {
-  // Si resultado ya tiene contenido, añadimos un guión antes de concatenar
+//while (ingreso !== null) {
+// Si resultado ya tiene contenido, añadimos un guión antes de concatenar
+// if (resultado !== "") {
+//   resultado += " - ";
+//}
+//añadimos lo ingresado a resultado
+// resultado += ingreso;
+// ingreso = prompt("Introduce otra cadena de texto (pulsa Cancelar para salir):");
+//}
+
+//if (resultado !== "") {
+// alert("Cadenas concatenadas: " + resultado);
+//} else {
+// alert("No se introdujeron cadenas.");
+//}
+
+//Realiza un script que pida cadenas de texto hasta que se pulse “cancelar”. Al salir con“cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+let resultado = "";
+let texto = prompt("Ingrese una cadena de texto, pulse cancelar para salir");
+
+while (texto !== null) {
   if (resultado !== "") {
     resultado += " - ";
   }
-  //añadimos lo ingresado a resultado
-  resultado += ingreso;
-  ingreso = prompt("Introduce otra cadena de texto (pulsa Cancelar para salir):");
-}
+  resultado += texto;
+  texto = prompt("Ingrese otra cadena de texto, pulse cancelar para salir");
+  }
+  if (resultado !== "") {
+    alert("las cadenas son: " + resultado);
+  } else {
+    alert("no hay cadenas concatenadas");
+  }
 
-if (resultado !== "") {
-  alert("Cadenas concatenadas: " + resultado);
-} else {
-  alert("No se introdujeron cadenas.");
-}
+
+

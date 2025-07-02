507422dc6d08ffddfbd006af847d3ca9f08b3727
Gonzalo Garcia <garciago@gestion.com>
2025-07-02 11:27:37 -0300

    se agrega ej. 8

---
+++ js/08-bucles.js
@@ -0,0 +1,17 @@
+// Crea script para generar pirámide siguiente con los números del 1 al número
+// que indique el usuario (no mayor de 50)
+// 1
+// 12
+// 123
+// 1234
+// 12345
+// 123456
+
+for(let i=1;i<=6;i++){
+    let linea = "";
+    for(let caracteres=1;caracteres<=i;caracteres++){
+linea +=caracteres
+    }
+    document.writeln(linea+"<br>")}
+
+
